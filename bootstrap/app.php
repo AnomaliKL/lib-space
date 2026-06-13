e8400474b0f9ce8e02d8a306f2ec48f3d4504c85
AnomaliKL <khaikalanjingbangsat@gmail.com>
2026-06-13 12:14:30 +0700

    fix: belokkan storage dan cache ke folder tmp vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,12 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Bootstrap\LoadConfiguration;
+use Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -15,4 +17,19 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->create();
+
+// === TAMBAHKAN KODE INI DI BAGIAN PALING BAWAH (Sebelum return $app) ===
+if (isset($_SERVER['VERCEL_JOB_ID']) || isset($_SERVER['NOW_REGION'])) {
+    $app->useStoragePath('/tmp/storage');
+    $app->bootstrapWith([
+        LoadEnvironmentVariables::class,
+        LoadConfiguration::class,
+    ]);
+
+    // Paksa jalur manifest cache Laravel ke folder /tmp yang writable
+    config(['app.manifest' => '/tmp/bootstrap/cache']);
+}
+
+return $app;
