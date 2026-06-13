44911c178a0c8f65894da17d92301c732b89c9b9
AnomaliKL <khaikalanjingbangsat@gmail.com>
2026-06-13 12:17:28 +0700

    fix: optimasi bootstrap cache override di api entry point

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,12 +1,10 @@
 <?php
 
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Bootstrap\LoadConfiguration;
-use Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-$app = Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -19,17 +17,3 @@ $app = Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->create();
-
-// === TAMBAHKAN KODE INI DI BAGIAN PALING BAWAH (Sebelum return $app) ===
-if (isset($_SERVER['VERCEL_JOB_ID']) || isset($_SERVER['NOW_REGION'])) {
-    $app->useStoragePath('/tmp/storage');
-    $app->bootstrapWith([
-        LoadEnvironmentVariables::class,
-        LoadConfiguration::class,
-    ]);
-
-    // Paksa jalur manifest cache Laravel ke folder /tmp yang writable
-    config(['app.manifest' => '/tmp/bootstrap/cache']);
-}
-
-return $app;
