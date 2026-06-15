9a35a33a04c9ef7b88f01c1102e22ac81793403c
AnomaliKL <khaikalanjingbangsat@gmail.com>
2026-06-15 09:29:47 +0700

    cache

--- backup_lokal.sql
+++ backup_lokal.sql
@@ -16,6 +16,37 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `book_category`
+--
+
+DROP TABLE IF EXISTS `book_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `book_category` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `book_id` bigint unsigned NOT NULL,
+  `category_id` bigint unsigned NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `book_category_book_id_foreign` (`book_id`),
+  KEY `book_category_category_id_foreign` (`category_id`),
+  CONSTRAINT `book_category_book_id_foreign` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `book_category_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book_category`
+--
+
+LOCK TABLES `book_category` WRITE;
+/*!40000 ALTER TABLE `book_category` DISABLE KEYS */;
+INSERT INTO `book_category` VALUES (1,1,1,NULL,NULL),(2,2,2,NULL,NULL),(3,2,1,NULL,NULL),(4,3,3,NULL,NULL);
+/*!40000 ALTER TABLE `book_category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `books`
 --
 
@@ -26,7 +57,6 @@ CREATE TABLE `books` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `author` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `category` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `stock` int NOT NULL DEFAULT '0',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
@@ -40,7 +70,7 @@ CREATE TABLE `books` (
 
 LOCK TABLES `books` WRITE;
 /*!40000 ALTER TABLE `books` DISABLE KEYS */;
-INSERT INTO `books` VALUES (1,'Belajar Laravel 11 Pemula sampai Mahir','Ahmad Fauzi','Framework',4,'2026-06-12 20:31:18','2026-06-12 21:05:57'),(2,'Mastering React & Tailwind CSS','Siti Rahma','Frontend',2,'2026-06-12 20:31:18','2026-06-12 20:31:18'),(3,'SQL & PostgreSQL Fundamentals','Rian Hidayat','Database',0,'2026-06-12 20:31:18','2026-06-12 20:31:18');
+INSERT INTO `books` VALUES (1,'Belajar Laravel 11 Pemula sampai Mahir','Ahmad Fauzi',4,'2026-06-14 18:54:43','2026-06-14 18:58:18'),(2,'Mastering React & Tailwind CSS','Siti Rahma',2,'2026-06-14 18:54:43','2026-06-14 18:54:43'),(3,'SQL & PostgreSQL Fundamentals','Rian Hidayat',0,'2026-06-14 18:54:43','2026-06-14 18:54:43');
 /*!40000 ALTER TABLE `books` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -67,7 +97,7 @@ CREATE TABLE `borrowings` (
   KEY `borrowings_book_id_foreign` (`book_id`),
   CONSTRAINT `borrowings_book_id_foreign` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE,
   CONSTRAINT `borrowings_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -76,7 +106,7 @@ CREATE TABLE `borrowings` (
 
 LOCK TABLES `borrowings` WRITE;
 /*!40000 ALTER TABLE `borrowings` DISABLE KEYS */;
-INSERT INTO `borrowings` VALUES (1,2,1,'2026-06-10','2026-06-17','2026-06-13',0,'Returned','2026-06-12 20:31:18','2026-06-12 21:05:40'),(2,3,1,'2026-06-13','2026-06-20','2026-06-13',0,'Returned','2026-06-12 20:31:18','2026-06-12 21:05:57'),(3,4,2,'2026-06-05','2026-06-12','2026-06-11',0,'Returned','2026-06-12 20:31:18','2026-06-12 20:31:18'),(4,5,3,'2026-06-01','2026-06-08','2026-06-13',10000,'Returned','2026-06-12 20:31:18','2026-06-12 20:31:18');
+INSERT INTO `borrowings` VALUES (1,2,1,'2026-06-10','2026-06-17','2026-06-15',0,'Returned','2026-06-14 18:54:43','2026-06-14 18:58:18'),(2,3,1,NULL,'2026-06-14',NULL,0,'Booking','2026-06-14 18:54:43','2026-06-14 18:54:43');
 /*!40000 ALTER TABLE `borrowings` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -126,6 +156,35 @@ CREATE TABLE `cache_locks` (
 LOCK TABLES `cache_locks` WRITE;
 /*!40000 ALTER TABLE `cache_locks` DISABLE KEYS */;
 /*!40000 ALTER TABLE `cache_locks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categories` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `categories_name_unique` (`name`),
+  UNIQUE KEY `categories_slug_unique` (`slug`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'Framework','framework','2026-06-14 18:54:43','2026-06-14 18:54:43'),(2,'Frontend','frontend','2026-06-14 18:54:43','2026-06-14 18:54:43'),(3,'Database','database','2026-06-14 18:54:43','2026-06-14 18:54:43');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -229,7 +288,7 @@ CREATE TABLE `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -238,7 +297,7 @@ CREATE TABLE `migrations` (
 
 LOCK TABLES `migrations` WRITE;
 /*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
-INSERT INTO `migrations` VALUES (1,'0001_01_01_000000_create_users_table',1),(2,'0001_01_01_000001_create_cache_table',1),(3,'0001_01_01_000002_create_jobs_table',1),(4,'2026_06_13_031904_create_books_table',1),(5,'2026_06_13_031912_create_borrowings_table',1);
+INSERT INTO `migrations` VALUES (1,'0001_01_01_000000_create_users_table',1),(2,'0001_01_01_000001_create_cache_table',1),(3,'0001_01_01_000002_create_jobs_table',1),(4,'2026_06_12_000000_create_categories_table',1),(5,'2026_06_13_031904_create_books_table',1),(6,'2026_06_13_031912_create_borrowings_table',1),(7,'2026_06_15_014433_create_book_category_table',1);
 /*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -292,7 +351,7 @@ CREATE TABLE `sessions` (
 
 LOCK TABLES `sessions` WRITE;
 /*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
-INSERT INTO `sessions` VALUES ('ulNhAF0h8vg58s2US45eX9QhOAjoaGIpNe2i1vL7',NULL,'127.0.0.1','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36','YTozOntzOjY6Il90b2tlbiI7czo0MDoiM0pIaUZrbFZxVXcwd2F0MWpZZXRCdGsxSkxmSDBJbGRkNHR6MVlSMCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly9saWItc3BhY2UudGVzdC9hZG1pbiI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=',1781323574);
+INSERT INTO `sessions` VALUES ('pdOUf2xdeUWIs4YPfy6HshONYrS7HE1SdaxrWi41',NULL,'127.0.0.1','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36','YTozOntzOjY6Il90b2tlbiI7czo0MDoick5aMGxXYVNtY29EQTBRNjVzcU5tRXlyRE9mbkpyTUg2bWI0QmxCOSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly9saWItc3BhY2UudGVzdC9hZG1pbiI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=',1781489174);
 /*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -318,7 +377,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`),
   UNIQUE KEY `users_member_code_unique` (`member_code`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -327,7 +386,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,NULL,'Khaikal Iksanuddin','khaikal@admin.com',NULL,'$2y$12$A9kvPS4RZVqY3RZnjUDYyeDSPmbqh9XMixYiRlkKzOV3FHlctsIzu','Admin','Active',NULL,'2026-06-12 20:31:16','2026-06-12 20:31:16'),(2,'#USR-001','Diva Oryza Sativa','diva@example.com',NULL,'$2y$12$pzOC8yBb8tKBdBjy2fO72OeRrSXF2WmkdA1j6CNc.ZWJb7CBeYsYq','Member','Active',NULL,'2026-06-12 20:31:17','2026-06-12 20:31:17'),(3,'#USR-002','Reza Aditya','reza@example.com',NULL,'$2y$12$Za3gABO2WhTPpINVVqrHxedwHIMV4/JfJa1XXlMW0SlEdBvGvtA.y','Member','Active',NULL,'2026-06-12 20:31:17','2026-06-12 20:31:17'),(4,'#USR-003','Siva Aprillia','siva@example.com',NULL,'$2y$12$1rF0O7Vj4INdaO5yvI4nneJcDFqU/pVi0t1AZnDN35igs0/uZQCSu','Member','Active',NULL,'2026-06-12 20:31:18','2026-06-12 20:31:18'),(5,'#USR-004','Helgi Destian','helgi@example.com',NULL,'$2y$12$m9XTgtpSRSgNvo/xruy9zupLkTcTkOPOOCgK7/Nit0IzifiSLlVf2','Member','Active',NULL,'2026-06-12 20:31:18','2026-06-12 20:31:18');
+INSERT INTO `users` VALUES (1,NULL,'Khaikal Iksanuddin','khaikal@admin.com',NULL,'$2y$12$MWvEbGF43t1Kcgb8blPROuMWTRiZkIgGChVFASP1FE244lp.e1uc.','Admin','Active',NULL,'2026-06-14 18:54:42','2026-06-14 18:54:42'),(2,'#USR-001','Diva Oryza Sativa','diva@example.com',NULL,'$2y$12$6qZqfNrbV3YKYr18XH6HcuCJN3gsyLsIKAp9k2/oUjpr0H01IsoHa','Member','Active',NULL,'2026-06-14 18:54:42','2026-06-14 18:54:42'),(3,'#USR-002','Reza Aditya','reza@example.com',NULL,'$2y$12$U7vdsv0VS2N5Bg4mO/KLAO4hlsP9F9870A0YyFTOv4ZZvY7nfHoyG','Member','Active',NULL,'2026-06-14 18:54:43','2026-06-14 18:54:43');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -340,4 +399,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-06-13 11:50:08
+-- Dump completed on 2026-06-15  9:20:58
